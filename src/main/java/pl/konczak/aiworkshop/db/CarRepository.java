1f4358e155459e919b7fefa45b0a21b2fa411603
TTIT\konczakp <piotr.konczak@ttpsc.pl>
2023-11-29 12:09:39 +0100

    feat: add Car db stuff

---
+++ src/main/java/pl/konczak/aiworkshop/db/CarRepository.java
@@ -0,0 +1,14 @@
+package pl.konczak.aiworkshop.db;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+    List<Car> findByBrand(String brand);
+
+    List<Car> findByModel(String model);
+
+    List<Car> findByBrandAndModel(String brand, String model);
+}
