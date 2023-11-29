d42d595da5845ceb34c87fe0066f0f26af2349f1
TTIT\konczakp <piotr.konczak@ttpsc.pl>
2023-11-29 11:06:27 +0100

    feat: upload project init by Spring Initializr
    - note: project does not start due to lack of db config

---
+++ src/main/java/pl/konczak/aiworkshop/AiWorkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.konczak.aiworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWorkshopApplication.class, args);
+	}
+
+}
