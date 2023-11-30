e6fa3fde80d4239263406e194b69e7f2f9519da0
TTIT\konczakp <piotr.konczak@ttpsc.pl>
2023-11-30 14:08:20 +0100

    feat: setup db stuff

---
+++ src/main/java/pl/konczak/aiworkshop/db/Car.java
@@ -0,0 +1,28 @@
+package pl.konczak.aiworkshop.db;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
+    @Column(name = "car_year")
+    private int year;
+
+}
